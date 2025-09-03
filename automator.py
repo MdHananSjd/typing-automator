45f6839a7215f997522bb35b3485951c6912b97d
Md. Hanan Sajid <mdhanansjd+github@gmail.com>
2025-09-03 09:21:35 +0530

    updates

--- automator.py
+++ automator.py
@@ -1,8 +1,8 @@
 import pyautogui as pg
 import time
 
-print("Typing will start in 3 seconds...")
-time.sleep(3)
+print("Typing will start in 5 seconds...")
+time.sleep(5)
 
 text_to_write = '''
 Enter the test you wanna type here
